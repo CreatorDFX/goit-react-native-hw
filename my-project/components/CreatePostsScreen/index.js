97ad5367a11310d33244c5ac2bede50b30ad1498
Alina Romanova <alinakhraban@gmail.com>
2023-03-27 21:41:08 +0300

    added screens styles

--- my-project/components/CreatePostsScreen/index.js
+++ my-project/components/CreatePostsScreen/index.js
@@ -1,52 +1,100 @@
-import { Text, View, StyleSheet, Dimensions, TouchableOpacity, Image, TextInput } from "react-native";
-import { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import { useState, useRef } from "react";
+
 const initialPostState = {
   name: "",
   location: "",
 };
-export default function CreatePostsScreen () {
+export default function CreatePostsScreen() {
   const [values, setValues] = useState(initialPostState);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const locationRef = useRef();
 
   return (
     <>
-    <View style={styles.postContainer}>
-      <Text style={styles.postTitle}>Create publication</Text>
-      <TouchableOpacity  style={styles.logoutBtn}>
-      <Image
-          style={styles.logoutIcon}
-          source={require('../../assets/image/arrow-left.png')}
-        />
-      </TouchableOpacity>
-    </View>
-    <View style = {styles.postFormWrap}>
-     <TextInput
-                value={values.name}
-                onChangeText={(value) =>
-                  setValues((prevState) => ({ ...prevState, name: value }))
-                }
-                placeholder="Name"
-                style={styles.input}
-                returnKeyType="next"
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Create publication</Text>
+        <TouchableOpacity style={styles.logoutBtn}>
+          <Image
+            style={styles.logoutIcon}
+            source={require("../../assets/image/arrow-left.png")}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.uploadWrap}>
+        <View style={styles.uploadPhoto}>
+          <Image
+            style={styles.uploadIcon}
+            source={require("../../assets/image/uploadIcon.png")}
+          />
+        </View>
+        <TouchableOpacity style={styles.uploadBtn}>
+          <Text style={styles.uploadBtnText}>Upload photo</Text>
+        </TouchableOpacity>
+
+        <View style={styles.postFormWrap}>
+          <TextInput
+            value={values.name}
+            onChangeText={(value) =>
+              setValues((prevState) => ({ ...prevState, name: value }))
+            }
+            placeholder="Name"
+            style={styles.input}
+            returnKeyType="next"
+            onFocus={() => setIsShowKeyboard(true)}
+            onSubmitEditing={() => {
+              locationRef.current.focus();
+            }}
+          />
+          <View style={{ marginTop: 16 }}>
+            <TextInput
+              ref={locationRef}
+              value={values.location}
+              onChangeText={(value) =>
+                setValues((prevState) => ({ ...prevState, location: value }))
+              }
+              placeholder="Location"
+              style={styles.input}
+            />
+            <View style={styles.locationPoint}>
+              <Image
+                style={styles.locationIcon}
+                source={require("../../assets/image/map-pin.png")}
               />
-               <TextInput
-                value={values.location}
-                onChangeText={(value) =>
-                  setValues((prevState) => ({ ...prevState, location: value }))
-                }
-                placeholder="Location"
-                style={styles.input}
-                returnKeyType="next"
-              />
-     </View>
-     </>
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.uploadFormBtn}>
+          <Text style={styles.uploadFormBtnText}>Publish</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.removeBtn}>
+          <Image
+            source={require("../../assets/image/trash-2.png")}
+          />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  uploadWrap: {
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
   postContainer: {
     justifyContent: "flex-end",
     width: Dimensions.get("window").width,
-    backgroundColor: 'white', 
+    backgroundColor: "white",
     height: 88,
     padding: 11,
     borderWidth: 1,
@@ -59,7 +107,6 @@ const styles = StyleSheet.create({
     lineHeight: 22,
     letterSpacing: -0.408,
     color: " #212121",
-   
   },
   logoutBtn: {
     padding: 10,
@@ -76,16 +123,66 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
-    alignItems: "center",
-
   },
   input: {
     width: 343,
     height: 50,
-    padding: 16,
     borderBottomWidth: 1,
     borderColor: "#E8E8E8",
     backgroundColor: "white",
-    
+  },
+  uploadBtn: {
+    height: 19,
+    marginBottom: 32,
+    justifyContent: "flex-start",
+    marginTop: 8,
+    borderColor: "transparent",
+    backgroundColor: "white",
+  },
+  uploadPhoto: {
+    height: 240,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+  },
+  uploadBtnText: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+    fontFamily: "Roboto-Regular",
+  },
+  locationPoint: {
+    position: "absolute",
+    top: 12,
+    left: 0,
+  },
+  locationIcon: {
+    marginRight: 10,
+  },
+  uploadFormBtn: {
+    height: 51,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    borderColor: "transparent",
+    backgroundColor: "#F6F6F6",
+    marginTop: 160,
+  },
+  uploadFormBtnText: {
+    textAlign: "center",
+    fontSize: 16,
+    color: "#BDBDBD",
+    fontFamily: "Roboto-Regular",
+  },
+  removeBtn: {
+    width: 70,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    borderColor: "transparent",
+    backgroundColor: "#F6F6F6",
+    marginTop: 107,
   },
 });
